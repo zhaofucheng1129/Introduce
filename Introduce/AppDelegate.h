28b688665117a05655286cefd15fa9c4c3a9079c
Tommy <zhaofucheng1129@gmail.com>
2015-01-19 10:48:26 +0800

    Initial Commit

---
+++ Introduce/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Introduce
+//
+//  Created by ZhaoFucheng on 15-1-19.
+//  Copyright (c) 2015年 ZhaoFucheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Introduce/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Introduce
+//
+//  Created by ZhaoFucheng on 15-1-19.
+//  Copyright (c) 2015年 ZhaoFucheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
